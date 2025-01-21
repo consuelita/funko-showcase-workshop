0f1a17891dfa07255ea4e7d02d6ee6821cf7274a
consuelita <adelita@operatorfoundation.org>
2025-01-21 15:17:46 -0600

    Update EditFunkoButton.tsx

--- start/components/EditFunkoButton/EditFunkoButton.tsx
+++ start/components/EditFunkoButton/EditFunkoButton.tsx
@@ -50,11 +50,11 @@ const EditFunkoButton = ({ setFunkos, funko }: { setFunkos: React.Dispatch<React
         <DialogContent>
 
           {/* Add Text Fields here */}
-          <TextField margin="normal" name="" label="" type="" fullWidth value={funko.character} onChange={handleInputChange}/>
-          <TextField margin="normal" name="" label="" type="" fullWidth value={funko.imageUrl} onChange={handleInputChange}/>
-          <TextField margin="normal" name="" label="" type="" fullWidth value={funko.numberInLine} onChange={handleInputChange}/>
-          <TextField margin="normal" name="" label="" type="" fullWidth value={funko.yearReleased} onChange={handleInputChange}/>
-          <TextField margin="normal" name="" label="" type="" fullWidth value={funko.source} onChange={handleInputChange}/>
+          <TextField margin="normal" name="" label="character" type="" fullWidth value={funko.character} onChange={handleInputChange}/>
+          <TextField margin="normal" name="" label="imageUrl" type="" fullWidth value={funko.imageUrl} onChange={handleInputChange}/>
+          <TextField margin="normal" name="" label="numberInLine" type="" fullWidth value={funko.numberInLine} onChange={handleInputChange}/>
+          <TextField margin="normal" name="" label="yearReleased" type="" fullWidth value={funko.yearReleased} onChange={handleInputChange}/>
+          <TextField margin="normal" name="" label="source" type="" fullWidth value={funko.source} onChange={handleInputChange}/>
 
         </DialogContent>
         <DialogActions>
